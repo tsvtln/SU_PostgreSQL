3303f8ea94ad478776f9cb3aed53c17b15c1451d
tsvtln <tsvtln@abv.bg>
2024-05-23 17:49:43 +0300

    Update 07_set_default.sql

---
+++ 001_Data_Types_and_Table_Basics/Exercise/07_set_default.sql
@@ -0,0 +1,8 @@
+alter table minions_info
+	alter column age set default 0;
+
+alter table minions_info
+	alter column name set default '';
+
+alter table minions_info
+	alter column code set default '';
