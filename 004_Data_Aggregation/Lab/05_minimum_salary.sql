89e28ea98b30b42f59f05a14061c4f89aa3068dc
tsvtln <tsvtln@abv.bg>
2024-05-27 17:05:24 +0300

    Update 05_minimum_salary.sql

--- 004_Data_Aggregation/Lab/05_minimum_salary.sql
+++ 004_Data_Aggregation/Lab/05_minimum_salary.sql
@@ -1,1 +1,1 @@
-select department_id, max(salary) from employees group by department_id order by department_id;
+select department_id, min(salary) from employees group by department_id order by department_id;
