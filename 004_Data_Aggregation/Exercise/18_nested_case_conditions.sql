645c8163a95a23ffced3b9bd373207f102079210
tsvtln <tsvtln@abv.bg>
2024-05-27 18:11:41 +0300

    Update 18_nested_case_conditions.sql

---
+++ 004_Data_Aggregation/Exercise/18_nested_case_conditions.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW
+			view_performance_rating
+AS SELECT
+		  e.first_name,
+		  e.last_name,
+		  e.job_title,
+		  e.salary,
+		  e.department_id,
+		  CASE
+		  	   WHEN salary >= 25000 THEN
+			   		CASE
+						 WHEN job_title LIKE 'Senior%' THEN 'High-performing Senior'
+						 WHEN job_title NOT LIKE 'Senior%' THEN 'High-performing Employee'
+						 ELSE 'Average-performing'
+					END
+			   ELSE 'Average-performing'
+		  END AS "performance_rating"
+FROM employees AS e;
