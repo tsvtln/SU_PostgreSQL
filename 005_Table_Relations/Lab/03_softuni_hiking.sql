10d73fd725ca38192a2e953700a99c67bf4a5592
tsvtln <tsvtln@abv.bg>
2024-05-27 18:34:54 +0300

    Update 03_softuni_hiking.sql

---
+++ 005_Table_Relations/Lab/03_softuni_hiking.sql
@@ -0,0 +1,10 @@
+select
+    routes.start_point,
+    routes.end_point,
+    routes.leader_id,
+    concat(campers.first_name, ' ', campers.last_name)
+from
+    routes
+join
+    campers on routes.leader_id = campers.id;
+     
