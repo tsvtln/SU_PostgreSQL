c603d760c5e4932c75c823bd78089a9b156a7299
tsvtln <tsvtln@abv.bg>
2024-06-13 14:34:59 +0300

    Update 01_user-defined_function_full_name.sql

--- 007_Database_Programmability/Exercise/01_user-defined_function_full_name.sql
+++ 007_Database_Programmability/Exercise/01_user-defined_function_full_name.sql
@@ -9,6 +9,7 @@ full_name varchar(255);
             initcap(concat(first_name, ' ', last_name))
         into
             full_name;
+        -- full_name := initcap(firstname) || ' ' || initcap(lower(last_name))
         return full_name;
     END;
 $$
